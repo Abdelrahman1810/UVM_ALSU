9064d2d9b828b34d1a8d4cca223c39ad5b59b92c
Abdelrahman M.Ali <abdelrahmansalby23@gmail.com>
2024-05-21 22:52:31 +0300

    reorgnize repo

--- run.tcl
+++ run.tcl
@@ -1,32 +1,32 @@
 vlib work
 
-vlog -coveropt 3 +cover +acc {shared_pkg\shared_pkg.sv}
+vlog -coveropt 3 +cover +acc {Codes\shared_pkg\shared_pkg.sv}
 ###
-vlog -coveropt 3 +cover +acc {Interface\interface.sv}
-vlog -coveropt 3 +cover +acc {Interface\ALSU_ref.sv}
+vlog -coveropt 3 +cover +acc {Codes\Interface\interface.sv}
+vlog -coveropt 3 +cover +acc {Codes\Interface\ALSU_ref.sv}
 ###
-vlog -coveropt 3 +cover +acc {DUT\ALSU.sv}
-vlog -coveropt 3 +cover +acc {DUT\assertion.sv}
+vlog -coveropt 3 +cover +acc {Codes\DUT\ALSU.sv}
+vlog -coveropt 3 +cover +acc {Codes\DUT\assertion.sv}
 ###
-vlog -coveropt 3 +cover +acc {objects\configration.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\configration.sv}
 
-vlog -coveropt 3 +cover +acc {objects\sequence_Items\sequenceItem.sv}
-vlog -coveropt 3 +cover +acc {objects\sequence_Items\sequenceItem_Valid.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\sequence_Items\sequenceItem.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\sequence_Items\sequenceItem_Valid.sv}
 
-vlog -coveropt 3 +cover +acc {objects\ALSU_sequence\ALSU_reset_sequence.sv}
-vlog -coveropt 3 +cover +acc {objects\ALSU_sequence\ALSU_main_sequence.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\ALSU_sequence\ALSU_reset_sequence.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\ALSU_sequence\ALSU_main_sequence.sv}
 ###
-vlog -coveropt 3 +cover +acc {UVM\Test\env\agent\driver\driver.sv}
-vlog -coveropt 3 +cover +acc {UVM\Test\env\agent\monitor\monitor.sv}
-vlog -coveropt 3 +cover +acc {UVM\Test\env\agent\sequencer\sequencer.sv}
-vlog -coveropt 3 +cover +acc {UVM\Test\env\agent\agent.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\env\agent\driver\driver.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\env\agent\monitor\monitor.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\env\agent\sequencer\sequencer.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\env\agent\agent.sv}
 ###
-vlog -coveropt 3 +cover +acc {UVM\Test\env\coverage_collector\coverage_collector.sv}
-vlog -coveropt 3 +cover +acc {UVM\Test\env\scoreboard\scoreboard.sv}
-vlog -coveropt 3 +cover +acc {UVM\Test\env\env.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\env\coverage_collector\coverage_collector.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\env\scoreboard\scoreboard.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\env\env.sv}
 ###
-vlog -coveropt 3 +cover +acc {UVM\Test\test.sv}
-vlog -coveropt 3 +cover +acc {UVM\top.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\Test\test.sv}
+vlog -coveropt 3 +cover +acc {Codes\UVM\top.sv}
 
 #vsim -voptargs=+acc work.top -classdebug -uvmcontrol=all
 vsim +UVM_VERBOSITY=UVM_LOW -voptargs=+acc work.top -classdebug -uvmcontrol=all -cover
--- run_extract_reports.tcl
+++ run_extract_reports.tcl
@@ -1,32 +1,32 @@
 vlib work
 
-vlog -coveropt 3 +cover +acc {shared_pkg\shared_pkg.sv}
+vlog -coveropt 3 +cover +acc {Codes\shared_pkg\shared_pkg.sv}
 ###
-vlog -coveropt 3 +cover +acc {Interface\interface.sv}
-vlog -coveropt 3 +cover +acc {Interface\ALSU_ref.sv}
+vlog -coveropt 3 +cover +acc {Codes\Interface\interface.sv}
+vlog -coveropt 3 +cover +acc {Codes\Interface\ALSU_ref.sv}
 ###
-vlog -coveropt 3 +cover +acc {design\ALSU.sv}
-vlog -coveropt 3 +cover +acc {design\assertion.sv}
+vlog -coveropt 3 +cover +acc {Codes\design\ALSU.sv}
+vlog -coveropt 3 +cover +acc {Codes\design\assertion.sv}
 ###
-vlog -coveropt 3 +cover +acc {objects\configration.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\configration.sv}
 
-vlog -coveropt 3 +cover +acc {objects\sequence_Items\sequenceItem.sv}
-vlog -coveropt 3 +cover +acc {objects\sequence_Items\sequenceItem_Valid.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\sequence_Items\sequenceItem.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\sequence_Items\sequenceItem_Valid.sv}
 
-vlog -coveropt 3 +cover +acc {objects\ALSU_sequence\ALSU_reset_sequence.sv}
-vlog -coveropt 3 +cover +acc {objects\ALSU_sequence\ALSU_main_sequence.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\ALSU_sequence\ALSU_reset_sequence.sv}
+vlog -coveropt 3 +cover +acc {Codes\objects\ALSU_sequence\ALSU_main_sequence.sv}
 ###
-vlog -coveropt 3 +cover +acc {top\Test\env\agent\driver\driver.sv}
-vlog -coveropt 3 +cover +acc {top\Test\env\agent\monitor\monitor.sv}
-vlog -coveropt 3 +cover +acc {top\Test\env\agent\sequencer\sequencer.sv}
-vlog -coveropt 3 +cover +acc {top\Test\env\agent\agent.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\env\agent\driver\driver.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\env\agent\monitor\monitor.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\env\agent\sequencer\sequencer.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\env\agent\agent.sv}
 ###
-vlog -coveropt 3 +cover +acc {top\Test\env\coverage_collector\coverage_collector.sv}
-vlog -coveropt 3 +cover +acc {top\Test\env\scoreboard\scoreboard.sv}
-vlog -coveropt 3 +cover +acc {top\Test\env\env.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\env\coverage_collector\coverage_collector.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\env\scoreboard\scoreboard.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\env\env.sv}
 ###
-vlog -coveropt 3 +cover +acc {top\Test\test.sv}
-vlog -coveropt 3 +cover +acc {top\top.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\Test\test.sv}
+vlog -coveropt 3 +cover +acc {Codes\top\top.sv}
 
 #vsim -voptargs=+acc work.top -classdebug -uvmcontrol=all
 vsim +UVM_VERBOSITY=UVM_LOW -voptargs=+acc work.top -classdebug -uvmcontrol=all -cover
